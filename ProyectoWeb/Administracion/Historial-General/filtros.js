787d027d0b1d68ac9f096c3a46a64a9f01767a75
JorScale <39716665+JorScale@users.noreply.github.com>
2019-04-24 15:35:51 -0600

    Botonoes en tabla
    se le puso iconos a los botones de imprimir,exel,pdf.

--- ProyectoWeb/Administracion/Historial-General/filtros.js
+++ ProyectoWeb/Administracion/Historial-General/filtros.js
@@ -1,21 +1,35 @@
-$(document).ready(function(){
-$('#mydata').DataTable({
-   dom: 'Bfrtip',
-    buttons: [
-            'excelHtml5',
-            'pdfHtml5'
-    ]   ,
-    "language":{
-        "sSearch": "Filtrar:",
-          "lengthMenu": "Mostrar _MENU_ registros",
-          "info":           "Mostrando _START_ a _END_ de _TOTAL_ registros",
-          "infoFiltered":   "(Filtro de _MAX_ total registros)",
-          "paginate": {
-        "first":      "Primero",
-        "last":       "Ultimo",
-        "next":       "Próximo",
-        "previous":   "Anterior"
-    }
-    }
-   });
+$(document).ready(function() {
+    $('#Historial').DataTable({
+        dom: 'Bfrtip',
+        buttons: [{
+                extend: 'excelHtml5',
+                text: '<i class="far fa-file-excel" id="excel-icon"></i>',
+                titleAttr: 'Excel'
+            },
+
+            {
+                extend: 'pdfHtml5',
+                text: '<i class="far fa-file-pdf" id="pdf-icon"></i>',
+                titleAttr: 'PDF'
+            },
+            {
+                extend: 'print',
+                text: ' <i class="fa fa-print" aria-hidden="true"></i>',
+                titleAttr: 'Imprimir'
+            }
+        ],
+        "language": {
+            "sSearch": "Filtrar:",
+            "info": "Mostrando _START_ a _END_ de _TOTAL_ registros",
+            "infoEmpty": "Mostrando 0 a 0 de 0 registros",
+            "infoFiltered": "(Filtro de _MAX_ total registros)",
+            "zeroRecords": "No se encontraron coincidencias",
+            "paginate": {
+                "first": "Primero",
+                "last": "Ultimo",
+                "next": "Próximo",
+                "previous": "Anterior"
+            }
+        }
+    });
 });
